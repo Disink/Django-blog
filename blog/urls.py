c8134a1f11049f94bb60828274b388905bb29787
disink <a07485748@gmail.com>
2019-10-28 17:42:40 +0800

    Add restful API Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from . import views
 from django.urls import path
 
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'post', views.PostViewSet)
+
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
     path('tag/<slug:slug>/', views.tag_detail, name='tag_detail'),
+    path('api/', include(router.urls)),
 ]
