20e45f7c963917c16779b22b3fe712ea0b7658e7
disink <a07485748@gmail.com>
2019-10-28 10:01:30 +0800

    Edit file name and path

---
+++ blog/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import Post, Comment, Tag
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('floor',
+                    'name',
+                    'body',
+                    'post',
+                    'created_on',
+                    'active',
+                    'replay')
+
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ("name",)
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
